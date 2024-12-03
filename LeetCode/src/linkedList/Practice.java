8caac9d47cdb25fe54970165074899123e53808e
prasad patil <prasadpatila99@gmail.com>
2024-12-03 21:52:06 +0530

    String

--- LeetCode/src/linkedList/Practice.java
+++ LeetCode/src/linkedList/Practice.java
@@ -1,5 +1,27 @@
 package linkedList;
 
 public class Practice {
+	public static String alternatelyString(String word1, String word2) {
+		StringBuilder merge=new StringBuilder();
+		int n1= word1.length();
+		int n2= word2.length();
+		int i=0;
+		while(i<n1 || i<n2) {
+			if(i<n1) {
+				merge.append(word1.charAt(i));
+			}
+			if(i<n2) {
+				merge.append(word2.charAt(i));
+				i++;
+			}
+		}
+		return merge.toString();
+	}
+	public static void main(String[] args) {
+		String w1= "HFJDHG";
+		String w2 =" mhhgh";
+		System.out.println(Practice.alternatelyString(w1, w2));
+	}
+
 
 }

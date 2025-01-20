0db183c9141ac4fbc27c5876527ddd8b92f0dda7
prasad patil <prasadpatila99@gmail.com>
2025-01-20 21:28:08 +0530

    stackc

--- LeetCode/src/basic_program/StackCollection.java
+++ LeetCode/src/basic_program/StackCollection.java
@@ -1,21 +1,28 @@
 package basic_program;
 
+import java.util.Iterator;
 import java.util.Stack;
 
 public class StackCollection {
-	
-	public static void main(String[] args) {
-		Stack<String>g=new Stack<String>();
-		g.push("hi");
-		g.push("darsh");
-		g.push("prasad");
-		g.push("roshan");
-		g.push("kunsh");
-		//System.out.println(g.get(1));
-		g.pop();
-		g.pop();
-		System.out.println(g);
-		
-	}
-
+    public static void main(String[] args) {
+        // Create a Stack and push elements onto it
+        Stack<String> g = new Stack<String>();
+        g.push("hi");
+        g.push("darsh");
+        g.push("prasad");
+        g.push("roshan");
+        g.push("kunsh");
+        
+        // Pop two elements from the stack
+    
+        
+        // Print the remaining elements in the stack
+       //System.out.println("Stack after pops: " + g);
+        
+        Iterator<String> itr=g.iterator();
+        while(itr.hasNext()) {
+        	System.out.println(itr.next());
+        }
+      
+    }
 }
